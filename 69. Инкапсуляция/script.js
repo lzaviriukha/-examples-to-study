e8df925d22bc07cd656ce7a55b1a29b8733e91ac
leozaviriukha <leozaviriukha@ukr.net>
2020-10-26 12:34:04 +0200

    constructor to class

--- 69. Инкапсуляция/script.js
+++ 69. Инкапсуляция/script.js
@@ -1,39 +1,40 @@
 'use strict';
 
-//пример на функции-конструкторе
+// пример на классах
 
-function User(name, age) {
-  this.name = name;
-  let userAge = age;
-
-  this.say = function () {
-    console.log(`Имя пользователя: ${this.name}, возраст ${userAge}`);
-  };
-
-  this.getAge = function() {
-    return userAge;
+class User {
+  constructor(name, age) {
+    this.name = name;
+    this.userAge = age;
   }
 
-  this.setAge = function(age) {
+  say() {
+    console.log(`Имя пользователя: ${this.name}, возраст ${this.userAge}`);
+  }
+
+  getAge() {
+    return this.userAge;
+  }
+
+  setAge(age) {
     if (typeof age === 'number' && age > 0 && age < 110) {
-      userAge = age;
+      this.userAge = age;
     } else {
       console.log('Недопустимое значение');
     }
-  };
+  }
 
 }
 
 const ivan = new User('Ivan', 27);
 console.log(ivan.name);
+ivan.userAge = 99;
 console.log(ivan.getAge());
 
 ivan.setAge(30);
 ivan.setAge(300);
-ivan.name = 'Alex';
 console.log(ivan.getAge());
 
 ivan.say();
 
 
-// пример на классах
